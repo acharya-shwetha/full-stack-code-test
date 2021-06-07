4bd7f49e3c7a35ed5feebe9ef781833e89ac2492
Shwetha Acharya <acharyashwetha94@gmail.com>
2021-06-07 16:01:46 +0530

    Adding mongo atlas connection for mongodb

--- booklisting_backend/config.js
+++ booklisting_backend/config.js
@@ -1,4 +1,5 @@
-var MONGO_URL = "mongodb://localhost:27017/BookListing";
+var MONGO_URL = "mongodb+srv://booklisting_writeaccess:pH2Ep0feJ9S1Z1fC@cluster0.6qxgm.mongodb.net/BookListing?retryWrites=true&w=majority";
+//"mongodb://localhost:27017/BookListing"
 
 module.exports = {
     MONGO_URL
